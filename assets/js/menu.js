1ec4037f71795693ca5ebaef970ab32b9b0c017e
mikewolfe83 <mgray2@ualr.edu>
2022-04-23 15:07:46 +0000

    group update

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,32 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
+function showMenu(menu) {
+
+    var angle = '0deg',
+      slide = '300px';
+  
+    if (menu) {
+      angle = '180deg';
+      slide = '0';
+    }
+  
+    // Slide panel
+    $("#menu__panel").css({
+      transform: "translateX(" + slide + ")"
+    });
+  
+    // Rotate icon
+    setTimeout(function() {
+      $("#menu__close").css({
+        transform: "rotate(" + angle + ")"
+      });
+    }, 300);
+  
+    // Animate menu items
+    $(".menu-item").each(function(i) {
+      var row = $(this);
+      setTimeout(function() {
+        menu && row.addClass('fadeInDown');
+        !menu && row.removeClass('fadeInDown');
+      }, 100 * i);
+    });
+  
+  }
